437f55dfc657550bdcfb95eba624df29a7ccbecf
Mohamed Zakaria <macbook@Mohameds-MacBook-Pro.local>
2021-09-14 11:47:24 +0200

    add project files

---
+++ pokedex3/pokedex3/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex3
+//
+//  Created by Mohamed Zakaria on 3/23/18.
+//  Copyright © 2018 Mohamed Zakaria . All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
---
+++ pokedex3/pokedex3/PokeCell.swift
@@ -0,0 +1,36 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Mohamed Zakaria on 3/23/18.
+//  Copyright © 2018 Mohamed Zakaria . All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    
+    func configureCell(_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+    
+    
+}
